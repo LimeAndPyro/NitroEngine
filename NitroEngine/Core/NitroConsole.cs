aab47016a21aee7717e76718b479bb3abc7512a9
Scope <98496609+ImKrazed@users.noreply.github.com>
2022-07-18 15:23:16 -0400

    Add project files.

---
+++ NitroEngine/Core/NitroConsole.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroEngine.Core
+{
+    internal class NitroConsole
+    {
+        public static void Msg(string message, bool consoletime = false)
+        {
+            if (consoletime)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("[");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(DateTime.Now.ToString("HH:mm:ss.fff"));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("] ");
+            }
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("NITRO");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" => ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(message + "\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Space(bool spacer)
+        {
+            if(spacer == true)
+            {
+                Console.WriteLine("");
+            }
+            else { }
+        }
+
+        public static void PrintArt()
+        {
+            //To be finished lol
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ NitroEngine/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using NitroEngine;
+
+[assembly: MelonInfo(typeof(NitroMain), "Nitro Engine", "1", "Scrim")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.Blue)]
+
+namespace NitroEngine
+{
+    public class NitroMain : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            
+        }
+
+        public override void OnUpdate()
+        {
+            
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            
+        }
+
+        public override void OnApplicationQuit()
+        {
+            
+        }
+    }
+}
